7c82499942e7fdccf9f7be8512bf4bca34f27043
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-04-01 14:15:23 +0900

    ✨ redirect機能追加

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,18 +1,26 @@
-import type { NextRequest } from 'next/server'
+import { type NextRequest, NextResponse } from 'next/server'
 import { auth0 } from '~/lib/auth0'
 
 export async function middleware(request: NextRequest) {
-	return await auth0.middleware(request)
+	const authResponse = await auth0.middleware(request)
+
+	const session = authResponse.cookies.get('__session')
+
+	if (session?.value) {
+		const url = new URL(request.nextUrl)
+		const redirectUrl = url.searchParams.get('redirectTo')
+
+		return NextResponse.redirect(redirectUrl ?? '')
+	}
+
+	return authResponse
 }
 
 export const config = {
 	matcher: [
-		/*
-		 * Match all request paths except for the ones starting with:
-		 * - _next/static (static files)
-		 * - _next/image (image optimization files)
-		 * - favicon.ico, sitemap.xml, robots.txt (metadata files)
-		 */
-		'/((?!_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)',
+		// Skip Next.js internals and all static files, unless found in search params
+		'/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+		// Always run for API routes
+		'/(api|trpc)(.*)',
 	],
 }
