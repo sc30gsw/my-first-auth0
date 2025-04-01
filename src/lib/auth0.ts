75ab27a1a92533f776d44c80ff13be544d7c9639
Kaito <74711118+sc30gsw@users.noreply.github.com>
2025-04-01 15:45:16 +0900

    🐛 middlewareのハンドリングを辞める

--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,3 +1,26 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server'
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
 
-export const auth0 = new Auth0Client()
+export const auth0 = new Auth0Client({
+	onCallback: async (error, ctx) => {
+		if (error) {
+			return NextResponse.json(
+				{ message: 'Authentication failed' },
+				{ status: 401 },
+			)
+		}
+
+		const cookiesStore = await cookies()
+
+		const redirectTo = cookiesStore.get('redirectTo')?.value ?? ''
+
+		const response = redirectTo
+			? NextResponse.redirect(redirectTo)
+			: NextResponse.redirect(process.env.APP_BASE_URL ?? '')
+
+		response.cookies.delete('redirectTo')
+
+		return response
+	},
+})
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,45 +1,19 @@
-import { type NextRequest, NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
 import { auth0 } from '~/lib/auth0'
 
 export async function middleware(request: NextRequest) {
 	const authResponse = await auth0.middleware(request)
 	const url = new URL(request.nextUrl)
-	let redirectUrl = url.searchParams.get('redirectTo')
-	// biome-ignore lint/suspicious/noConsoleLog: <explanation>
-	// biome-ignore lint/suspicious/noConsole: <explanation>
-	console.log('🚀 ~ middleware ~ redirectUrl:', redirectUrl)
+	const redirectUrl = url.searchParams.get('redirectTo')
 
 	if (redirectUrl) {
 		authResponse.cookies.set('redirectTo', redirectUrl, {
 			path: '/',
-			maxAge: 60 * 60 * 24, // 1 day
+			maxAge: 60 * 60 * 24,
 			httpOnly: true,
 			sameSite: 'lax',
 			secure: process.env.NODE_ENV === 'production',
 		})
-	} else {
-		redirectUrl = request.cookies.get('redirectTo')?.value ?? ''
-	}
-
-	const session = authResponse.cookies.get('__session')
-
-	if (session?.value) {
-		// biome-ignore lint/suspicious/noConsoleLog: <explanation>
-		// biome-ignore lint/suspicious/noConsole: <explanation>
-		console.log('🚀 ~ middleware ~ redirectUrl2:', redirectUrl)
-		// Cookie を削除（セキュリティ対策）
-		authResponse.cookies.delete('redirectTo')
-
-		try {
-			return NextResponse.redirect(redirectUrl)
-		} catch (err: unknown) {
-			if (err instanceof Error) {
-				// biome-ignore lint/suspicious/noConsoleLog: <explanation>
-				// biome-ignore lint/suspicious/noConsole: <explanation>
-				console.log('🚀 ~ middleware ~ err:', err, err.message)
-			}
-			return authResponse
-		}
 	}
 
 	return authResponse
